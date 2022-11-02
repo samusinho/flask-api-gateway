d5703bdb74f8147edace20e35056a1b8b1c3c6f2
Samuel Pérez <samusinho79@gmail.com>
2022-11-01 20:54:37 -0500

    se consumen servicios para creación de partidos políticos y su visualización

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from settings import URL, PORT
+import requests
+from settings import URL, PORT, VOTES_URL, SECURITY_URL
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,6 +12,42 @@ def ping():
     return jsonify({
         "message": "pong..."
     })
+
+
+@app.route("/parties", methods=["POST"])  # localhost:5001/parties (api-gateway)
+def create_party():
+    body = request.get_json()
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(
+        url=f"{VOTES_URL}/parties",  # localhost:5000/parties (ms votaciones)
+        json=body,
+        headers=headers
+    )
+    if response.status_code == 201:
+        return jsonify(response.json()), 201
+    else:  # status_code == 500
+        return jsonify({
+            "message": "Hubo un error al crear partido político"
+        }), 500
+
+
+@app.route("/parties", methods=["GET"])
+def parties():
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.get(
+        url=f"{VOTES_URL}/parties",  # localhost:5000/parties (ms votaciones)
+        headers=headers
+    )
+    if response.status_code == 200:
+        return jsonify(response.json()), 200
+    else:  # status_code == 500
+        return jsonify({
+            "message": "Hubo un error al obtener la lista de partidos"
+        }), 500
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 URL = os.environ.get("URL")
 PORT = int(os.environ.get("PORT"))
+VOTES_URL = os.environ.get("VOTES_URL")
+SECURITY_URL = os.environ.get("SECURITY_URL")
